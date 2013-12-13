7a38ea734c6be1e2dfb908873a6121b8f763a9b4
mynario <mynario@gmail.com>
2013-12-13 15:30:29 +0100

    alarm wake up

--- appliction/src/se/mah/kingdom/AlarmReceiver.java
+++ appliction/src/se/mah/kingdom/AlarmReceiver.java
@@ -3,14 +3,18 @@ package se.mah.kingdom;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
+    	 
         Toast.makeText(context, "Alarm went off", Toast.LENGTH_SHORT).show();
         Intent intent2 = new Intent(context, WakeUp.class);
         intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
         context.startActivity(intent2);
+
     } 
+
 }
--- appliction/src/se/mah/kingdom/WakeUp.java
+++ appliction/src/se/mah/kingdom/WakeUp.java
@@ -1,21 +1,31 @@
 package se.mah.kingdom;
 
+import java.util.zip.Inflater;
+
+import android.hardware.Camera.Parameters;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.provider.Settings;
 import android.app.Activity;
+import android.app.KeyguardManager;
+import android.app.KeyguardManager.KeyguardLock;
 import android.content.Context;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 
 public class WakeUp extends Activity {
 	private MediaPlayer event_player;
 	private AudioManager event_am;
+    private static PowerManager.WakeLock wakeLock;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_wake_up);
+		wakeDevice();
 		event_am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 		event_am.setMode(AudioManager.MODE_NORMAL);
 		event_am.setSpeakerphoneOn(true);
@@ -25,7 +35,24 @@ public class WakeUp extends Activity {
 		event_player.setAudioStreamType(AudioManager.STREAM_MUSIC);
 		event_player.start();
 	}
+    public void wakeDevice() {
+        PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+        wakeLock.acquire();
 
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardLock keyguardLock = keyguardManager.newKeyguardLock("TAG");
+        keyguardLock.disableKeyguard();
+        runOnUiThread(new Runnable(){
+            public void run(){
+                getWindow().addFlags(
+                          WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);                
+            }
+        });
+    }
 	public void onStart() {
 		event_player.start();
 		super.onStart();
