d0bc4617a1c394311463b555b608b45fe9513125
mynario <mynario@gmail.com>
2014-01-10 17:19:40 +0100

    changable map

--- kingdom_v02/src/se/mah/kingdom_v02/StoryManager.java
+++ kingdom_v02/src/se/mah/kingdom_v02/StoryManager.java
@@ -1,6 +1,7 @@
 package se.mah.kingdom_v02;
 
 import java.util.Calendar;
+import java.util.Random;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -9,8 +10,14 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class StoryManager extends Activity {
+	private int gold = 2;
+	private int food = 2;
+	private int happy = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,24 @@ public class StoryManager extends Activity {
 		time.setTimeInMillis(System.currentTimeMillis());
 		time.add(Calendar.SECOND, 5);
 		alarmMgr.set(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(),pendingIntent);
+		ImageView fl = (ImageView) findViewById(R.id.imageMapLF);
+		ImageView fm = (ImageView) findViewById(R.id.imageMapMF);
+		ImageView fh = (ImageView) findViewById(R.id.imageMapHF);
+		ImageView gl = (ImageView) findViewById(R.id.imageMapLG);
+		ImageView gm = (ImageView) findViewById(R.id.imageMapMG);
+		ImageView gh = (ImageView) findViewById(R.id.imageMapHG);
+		ImageView hl = (ImageView) findViewById(R.id.imageMapLH);
+		ImageView hm = (ImageView) findViewById(R.id.imageMapMH);
+		ImageView hh = (ImageView) findViewById(R.id.imageMapHH);
+		fl.setVisibility(4);
+		fm.setVisibility(4);
+		fh.setVisibility(4);
+		gl.setVisibility(4);
+		gm.setVisibility(4);
+		gh.setVisibility(4);
+		hl.setVisibility(4);
+		hm.setVisibility(4);
+		hh.setVisibility(4);
 	}
 
 	@Override 
@@ -30,6 +55,88 @@ public class StoryManager extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void changeStat (View v){
+		Random r = new Random();
+		gold=r.nextInt(3)+1;
+		food=r.nextInt(3)+1;
+		happy=r.nextInt(3)+1;
+		Button butt = (Button) findViewById(R.id.buttonChangeStat);
+		butt.setText(Integer.toString(gold));
+		
+		
+		ImageView fl = (ImageView) findViewById(R.id.imageMapLF);
+		ImageView fm = (ImageView) findViewById(R.id.imageMapMF);
+		ImageView fh = (ImageView) findViewById(R.id.imageMapHF);
+		ImageView gl = (ImageView) findViewById(R.id.imageMapLG);
+		ImageView gm = (ImageView) findViewById(R.id.imageMapMG);
+		ImageView gh = (ImageView) findViewById(R.id.imageMapHG);
+		ImageView hl = (ImageView) findViewById(R.id.imageMapLH);
+		ImageView hm = (ImageView) findViewById(R.id.imageMapMH);
+		ImageView hh = (ImageView) findViewById(R.id.imageMapHH);
+		
+		switch (gold){
+			case 1: {
+				gl.setVisibility(View.VISIBLE);
+				gm.setVisibility(View.GONE);
+				gh.setVisibility(View.GONE);
+				break;
+			}
+			case 2: {
+				gl.setVisibility(View.GONE);
+				gm.setVisibility(View.VISIBLE);
+				gh.setVisibility(View.GONE);
+				break;
+			}
+			case 3: {
+				gl.setVisibility(View.GONE);
+				gm.setVisibility(View.GONE);
+				gh.setVisibility(View.VISIBLE);
+				break;
+			}
+		}
+		
+		switch (food){
+		case 1: {
+			fl.setVisibility(View.VISIBLE);
+			fm.setVisibility(View.GONE);
+			fh.setVisibility(View.GONE);
+			break;
+		}
+		case 2: {
+			fl.setVisibility(View.GONE);
+			fm.setVisibility(View.VISIBLE);
+			fh.setVisibility(View.GONE);
+			break;
+		}
+		case 3: {
+			fl.setVisibility(View.GONE);
+			fm.setVisibility(View.GONE);
+			fh.setVisibility(View.VISIBLE);
+			break;
+		}
+	}
+		switch (happy){
+		case 1: {
+			hl.setVisibility(View.VISIBLE);
+			hm.setVisibility(View.GONE);
+			hh.setVisibility(View.GONE);
+			break;
+		}
+		case 2: {
+			hl.setVisibility(View.GONE);
+			hm.setVisibility(View.VISIBLE);
+			hh.setVisibility(View.GONE);
+			break;
+		}
+		case 3: {
+			hl.setVisibility(View.GONE);
+			hm.setVisibility(View.GONE);
+			hh.setVisibility(View.VISIBLE);
+			break;
+		}
+	}
 	}
 
 }
