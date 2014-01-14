b601a79dc9935511670189f1af315dbae21c1e7e
ma <mj.emeth@gmail.com>
2014-01-14 15:57:58 +0100

    New game

--- kingdom_v02/src/se/mah/kingdom_v02/StartEvent.java
+++ kingdom_v02/src/se/mah/kingdom_v02/StartEvent.java
@@ -1,8 +1,17 @@
 package se.mah.kingdom_v02;
 
+import java.util.Calendar;
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class StartEvent extends Activity {
 
@@ -11,5 +20,7 @@ public class StartEvent extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start_event);
 	}
-
+	public void newGame (View v) {
+		
+	}
 }
