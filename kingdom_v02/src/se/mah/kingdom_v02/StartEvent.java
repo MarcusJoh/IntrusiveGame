1dd680fc71b56b9a146f752fcd4d9039936cafab
ma <mj.emeth@gmail.com>
2014-01-14 13:58:08 +0100

    More Strings

---
+++ kingdom_v02/src/se/mah/kingdom_v02/StartEvent.java
@@ -0,0 +1,15 @@
+package se.mah.kingdom_v02;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class StartEvent extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_event);
+	}
+
+}
