1a8caee015e9804c296a3716721a84c2870d5a6b
Tim Tuvestam <maus_tim@hotmail.com>
2014-01-15 15:59:36 +0100

    Tim deathranking

--- kingdom_v02/src/se/mah/kingdom_v02/DeathEvent.java
+++ kingdom_v02/src/se/mah/kingdom_v02/DeathEvent.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import se.mah.kingdom_v02.R.id;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlarmManager;
@@ -15,6 +16,7 @@ import android.content.res.Resources;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DeathEvent extends Activity {
@@ -22,6 +24,8 @@ public class DeathEvent extends Activity {
 	TextView deathSentence;
 	String[] kings;
 	TextView kingText;
+	int numberInArray = 0;
+	Button button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,41 +34,84 @@ public class DeathEvent extends Activity {
 		
 		deathSentence = (TextView) findViewById(id.grave_headline);
 		kingText = (TextView) findViewById(id.king);
+		button = (Button) findViewById(id.fuckingbutton);
 		
 		Resources resStory = getResources();
 		kings = resStory.getStringArray(R.array.kings_ranking);
 		int ranking = ResourcesKingdom.CombinedResources();
 		
-		if(ranking <6)
-			kingText.setText(kings[14]);
-		else if(ranking <12&& ranking >=6)
+		if(ranking <12)
+		{
 			kingText.setText(kings[13]);
+			numberInArray = 13;
+		}
 		else if(ranking <18&& ranking >=12)
+		{
 			kingText.setText(kings[12]);
+			numberInArray = 12;
+		}
 		else if(ranking <24&& ranking >=18)
+		{
 			kingText.setText(kings[11]);
+			numberInArray = 11;
+		}
 		else if(ranking <30&& ranking >=24)
+		{
 			kingText.setText(kings[10]);
+			numberInArray = 10;
+		}
 		else if(ranking <36&& ranking >=30)
+		{
 			kingText.setText(kings[9]);
+			numberInArray = 9;
+		}
 		else if(ranking <42&& ranking >=36)
+		{
 			kingText.setText(kings[8]);
+			numberInArray = 8;
+		}
 		else if(ranking <48&& ranking >=42)
+		{
 			kingText.setText(kings[7]);
+			numberInArray = 7;
+		}
 		else if(ranking <54&& ranking >=48)
+		{
 			kingText.setText(kings[6]);
+			numberInArray = 6;
+		}
 		else if(ranking <60&& ranking >=54)
+		{
 			kingText.setText(kings[5]);
+			numberInArray = 5;
+		}
 		else if(ranking <66&& ranking >=60)
+		{
 			kingText.setText(kings[4]);
+			numberInArray = 4;
+		}
 		else if(ranking <72&& ranking >=66)
+		{
 			kingText.setText(kings[3]);
+			numberInArray = 3;
+		}
 		else if(ranking <78&& ranking >=72)
+		{
 			kingText.setText(kings[2]);
+			numberInArray = 2;
+		}
 		else if(ranking <84&& ranking >=78)
+		{
 			kingText.setText(kings[1]);
+			numberInArray = 1;
+		}
 		else if(ranking <100 && ranking >=84)
+		{
 			kingText.setText(kings[0]);
+			numberInArray = 0;
+		}
+		
+		button.setText("Klick to find out more about " + kings[numberInArray]);
 		
 		if (ResourcesKingdom.getFood() <= 0)
 			deathSentence.setText("You starved to death!");
@@ -83,6 +130,17 @@ public class DeathEvent extends Activity {
 		intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		DeathEvent.this.finish();
 		startActivity(intent2);
+	}
+	
+	public void Something(View v)
+	{
+		Resources resStory = getResources();
+		String[] webpage = resStory.getStringArray(R.array.kings_webpage);
+		Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse(webpage[numberInArray]));
+        startActivity(intent);
 	}
 
 }
